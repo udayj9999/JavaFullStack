a54f52238d84edef2007ff231fc72e8858cca1f7
udayj9999 <udayjadhav1407@gmail.com>
2021-02-23 15:27:01 +0530

    Test

---
+++ src/sample/Test.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Test {
+    public void addition(){
+        System.out.println("hello");
+
+    }
+    public int display(){
+        return 3;
+    }
+
+    public static void main(String[] args) {
+        Test test=new Test();
+        test.addition();
+        test.display();
+    }
+}
