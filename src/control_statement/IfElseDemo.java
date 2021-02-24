be90e97f8a55957e8afcde62f25c30c146531dca
udayj9999 <udayjadhav1407@gmail.com>
2021-02-24 16:54:51 +0530

    If Else

---
+++ src/control_statement/IfElseDemo.java
@@ -0,0 +1,31 @@
+package control_statement;
+
+public class IfElseDemo {
+
+    public static void main(String[] args) {
+//        int i=10;
+//        if(i>0){
+//            System.out.println("number is positive");
+//        }else{
+//            System.out.println("number is negative");
+//        }
+//    }
+//}
+//    int a=123,b=25,c=432;
+//
+//   if(a>b && a>c){
+//       System.out.println("a is greater");
+//           }else if(b>c){
+//       System.out.println("b is greater");
+//   }else{
+//       System.out.println("c greater");
+//
+//   }
+
+   //Nested if
+        int age = 18;
+        String nationality = "Indian";
+
+        if(age>=18){
+   }
+}
