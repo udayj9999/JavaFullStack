17919dd4ea4b0e0a4b8fbec6a629c77483b7b00d
udayj9999 <udayjadhav1407@gmail.com>
2021-02-25 15:00:12 +0530

    24/2 work

--- src/control_statement/IfElseDemo.java
+++ src/control_statement/IfElseDemo.java
@@ -3,16 +3,16 @@ package control_statement;
 public class IfElseDemo {
 
     public static void main(String[] args) {
-//        int i=10;
-//        if(i>0){
-//            System.out.println("number is positive");
-//        }else{
-//            System.out.println("number is negative");
-//        }
-//    }
-//}
-//    int a=123,b=25,c=432;
-//
+        int i=10;
+        if(i>0){
+            System.out.println("number is positive");
+        }else{
+            System.out.println("number is negative");
+        }
+    }
+}
+    int a=123,b=25,c=432;
+
 //   if(a>b && a>c){
 //       System.out.println("a is greater");
 //           }else if(b>c){
@@ -20,4 +20,22 @@ public class IfElseDemo {
 //   }else{
 //       System.out.println("c greater");
 //
+
+    //Nested if
+//    int age = 18;
+//    String nationality = "Indian";
+//
+//        if (age >= 18) {
+//                if (nationality == "Indian") {
+//                System.out.println("You can vote");
+//                } else {
+//                System.out.println("You are not indian");
+//                }
+//                } else {
+//                System.out.println("You can not vote");
+//                }
+//
+//                int s = i > 0 ? 100 : 200;
+//
+//                System.out.println(s);
 //   }
