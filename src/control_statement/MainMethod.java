22ca37447dfd2fba6cc0bfb3eb94e8acf3219ba6
udayj9999 <udayjadhav1407@gmail.com>
2021-03-01 10:23:24 +0530

    Star Print

--- src/control_statement/MainMethod.java
+++ src/control_statement/MainMethod.java
@@ -4,8 +4,8 @@ public class MainMethod {
     public static void main(String[] args) {
         for(int i=1; i<=5;i++)
         {
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+            for(int j=1;j<=5;j++){
+                System.out.print(" *");
             }
             System.out.println();
         }
