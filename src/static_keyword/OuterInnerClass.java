4505feeedc74be80e4d69eaea088b06ee275544a
udayj9999 <udayjadhav1407@gmail.com>
2021-03-16 17:11:26 +0530

    Static_Keyword

--- src/static_keyword/OuterInnerClass.java
+++ src/static_keyword/OuterInnerClass.java
@@ -1,2 +1,38 @@
-package static_keyword;public class OuterInnerClass {
+package static_keyword;
+
+public class OuterInnerClass {
+
+    public static void main(String[] args) {
+
+        Outer outer = new Outer();
+        outer.show();
+
+//        Outer.Inner inner = outer.new Inner();
+//        Outer.Inner inner1 = new Outer.Inner();
+    }
+
+}
+
+class Outer {
+
+    int i = 10;
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+        inner.innerShow();
+    }
+
+    public void show() {
+        System.out.println(i);
+    }
+
+    //static
+    private class Inner {
+        int i = 18;
+
+        public void innerShow() {
+            System.out.println(i);
+        }
+    }
 }
--- src/static_keyword/StaticKeywordDemo.java
+++ src/static_keyword/StaticKeywordDemo.java
@@ -1,2 +1,35 @@
-package static_keyword;public class StaticKeywordDemo {
+package static_keyword;
+
+public class StaticKeywordDemo {
+    static final String companyName;
+
+    static {
+        System.out.println("In static block");
+        companyName = "abc";
+    }
+
+    int id;
+    String name;
+
+    public static void show() {
+        System.out.println("In static method");
+    }
+
+    public static void main(String[] args) {
+
+        StaticKeywordDemo.show();
+
+        StaticKeywordDemo obj1 = new StaticKeywordDemo();
+        obj1.id = 1;
+        obj1.name = "sagar";
+
+        StaticKeywordDemo obj2 = new StaticKeywordDemo();
+        obj2.id = 2;
+        obj2.name = "Suhas";
+
+        System.out.println();
+        System.out.println("Id=>" + obj1.id + " Name=>" + obj1.name + " comapnyName=>" + companyName);
+        System.out.println("Id=>" + obj2.id + " Name=>" + obj2.name + " comapnyName=>" + companyName);
+    }
+
 }
